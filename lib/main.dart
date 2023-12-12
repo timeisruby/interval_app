1a951a72c1acb06e1a58b78627ddcacb444b5ba8
timeisruby <timeisruby@gmail.com>
2023-12-13 07:06:01 +0900

    timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:interval/screen/home_screen.dart';
 import 'package:interval/screen/splash_screen.dart';
+import 'package:interval/screen/timer_screen_2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/':(context) => SplashScreen(),
-        '/main':(context) => HomeScreen(),
+        '/main':(context) => TimerTwo(),
       },
     );
   }
--- lib/screen/timer_screen_2.dart
+++ lib/screen/timer_screen_2.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:timezone/timezone.dart';
 
-class TimerTwo extends StatelessWidget {
+class TimerTwo extends StatefulWidget {
   const TimerTwo({super.key});
 
   @override
+  State<TimerTwo> createState() => _TimerTwoState();
+}
+
+class _TimerTwoState extends State<TimerTwo> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Timer App"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.settings),
+          )
+        ],
+      ),
+      body: Center(
+        child: Text("App Content"),
+      ),
+    );
   }
 }

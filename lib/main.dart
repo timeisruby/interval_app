b9a20711ba9bb6ec306dfaf49c494cc57044743e
timeisruby <timeisruby@gmail.com>
2023-12-20 22:50:45 +0900

    home

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/':(context) => SplashScreen(),
-        '/main':(context) => TimerScreen(),
+        '/home':(context) => TimerScreen(),
       },
     );
   }

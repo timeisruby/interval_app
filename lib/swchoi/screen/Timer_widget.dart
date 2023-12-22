fe2bdbce7ec2d7487bbc358799e4d1846bfbc7f7
timeisruby <timeisruby@gmail.com>
2023-12-22 21:43:57 +0900

    add labTimer

--- lib/swchoi/screen/Timer_widget.dart
+++ lib/swchoi/screen/Timer_widget.dart
@@ -9,20 +9,21 @@ class TimerWidget extends StatefulWidget {
 }
 
 class _TimerWidgetState extends State<TimerWidget> {
-  int _seconds = 0;
+  int _milliseconds = 0;
   late Timer _timer;
   bool _isRunning = false;
+  List<String> labTimer = [];
 
   @override
   void initState() {
     super.initState();
-    _timer = Timer.periodic(Duration(seconds: 1), _onTimerTick);
+    _timer = Timer.periodic(Duration(milliseconds: 1), _onTimerTick);
   }
 
   void _onTimerTick(Timer timer) {
     if (_isRunning) {
       setState(() {
-        _seconds++;
+        _milliseconds++;
       });
     }
   }
@@ -35,29 +36,63 @@ class _TimerWidgetState extends State<TimerWidget> {
 
   void _resetTimer() {
     setState(() {
-      _seconds = 0;
-      _isRunning = false;
+      if (!_isRunning) {
+        labTimer.clear();
+      } else {
+        labTimer.clear();
+        _milliseconds = 0;
+        _isRunning = false;
+      }
     });
+  }
+
+  void _labTimer() {
+    setState(() {
+      labTimer.insert(0, formatTimer(_milliseconds));
+    });
+  }
+
+  String formatTimer(int milliseconds) {
+    Duration duration = Duration(milliseconds: milliseconds);
+    int seconds = duration.inSeconds % 60;
+    int minutes = (duration.inMinutes % 60);
+    int hours = duration.inHours;
+    return "${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}";
   }
 
   @override
   Widget build(BuildContext context) {
-    String formatTimer = "$_seconds";
+    String _formatTimer = formatTimer(_milliseconds);
     return Column(
       children: [
         Container(
-          child: Text(formatTimer,style: TextStyle(fontSize: 70),),
+          child: Text(
+            _formatTimer,
+            style: TextStyle(fontSize: 70),
+          ),
         ),
         Container(
-          child: buildButton(_startStopTimer,_resetTimer,_isRunning)
-        )
+            child: buildButton(
+                _startStopTimer, _resetTimer, _labTimer, _isRunning)),
+        SizedBox(height: 30),
+        Container(
+            height: 200,
+            child: ListView.builder(
+              physics: BouncingScrollPhysics(),
+              itemCount: labTimer.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text("${labTimer[index]}"),
+                );
+              },
+            ))
       ],
     );
   }
 }
 
-
-Widget buildButton(VoidCallback startStopCallback, VoidCallback resetCallback, bool isRunning){
+Widget buildButton(VoidCallback startStopCallback, VoidCallback resetCallback,
+    VoidCallback labTimerCallback, bool isRunning) {
   return Row(
     mainAxisAlignment: MainAxisAlignment.center,
     children: <Widget>[
@@ -70,6 +105,8 @@ Widget buildButton(VoidCallback startStopCallback, VoidCallback resetCallback, b
         onPressed: resetCallback,
         child: Text("Reset"),
       ),
+      SizedBox(width: 30),
+      ElevatedButton(onPressed: labTimerCallback, child: Text("Lab"))
     ],
   );
 }
